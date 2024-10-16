2eeb61670530a771fa95086777e1d598e3103718
efinden <141635350+efinden@users.noreply.github.com>
2024-10-16 12:17:36 +0200

    Update dielectric_data.py

--- dielectric_data.py
+++ dielectric_data.py
@@ -118,61 +118,12 @@ def data():
 
 	JSC_Mars_1_60_perc_ice_0_1M_CaCl2_210K = Dielectric(r'JSC Mars 1 w/ 60\% ice', 210-273, 1000, 5, 1/(2*np.pi*0.0333), 2e-9, .75, 125, 5, 1/(2*np.pi*3.33e-04), .75) # Data frin Simoes 2004, (also in agreement with Grimm 2007).  w/ 0.1 M CaCl$_2$
 
-
-
-	c_test = Dielectric(r'Testing c-exponent, c=1.0'          , 100-273, 3.1, 3.1, 1.0, 1e-30, 1.0, 278.0, 3.1, 1/(2*np.pi*1e02), 1.0) # Grimm 2008, Johari 1975. -->
-	c_test_c_0999 = Dielectric(r'Testing c-exponent, c=0.9999', 100-273, 3.1, 3.1, 1.0, 1e-30, 1.0, 278.0, 3.1, 1/(2*np.pi*1e02), 0.9999) # Grimm 2008, Johari 1975. -->
-	
-	
-	num_test_c_stab_c_1 = Dielectric(r'Num. test, c=1.0', 181-273, 1000, 4, 1/(2*np.pi*1e05), 1e-12, .7, 33, 4, 1/(2*np.pi*2e-03), 1.0) #
-	num_test_c_stab_c_0999 = Dielectric(r'Num. test, c=0.999', 181-273, 1000, 4, 1/(2*np.pi*1e05), 1e-12, .7, 33, 4, 1/(2*np.pi*2e-03), 0.999) #
-	num_test_c_stab_c_076 = Dielectric(r'Num. test', 181-273, 1000, 4, 1/(2*np.pi*1e05), 1e-12, .7, 33, 4, 1/(2*np.pi*1e-02), 0.76) # Grimm and Stillman 2010, 2011
-	
-	
-	
-	
-	
-	Bedrock = Dielectric('Bedrock', 10, 3, 3, 1, 1/100000, 1, 3, 3, 1, 1) # Dry sand or gravel have this, bok av  bok av Seidel, Lange. 2007 (Knodel e.d.) p. 238. 
-	Dry_sand = Dielectric('Dry sand', 10, 3, 3, 1, 0.01*1e-03, 1, 3, 3, 1, 1) # Dry sand or gravel have this, bok av  bok av Seidel, Lange. 2007 (Knodel e.d.) p. 311. 
-	Water_sat_high_sand = Dielectric('Water saturated sand (max)', 10, 30, 30, 1, 1*1e-03, 1, 30, 30, 1, 1) # Dry sand or gravel have this, bok av  bok av Seidel, Lange. 2007 (Knodel e.d.) p. 311. 
-	Water_sat_low_sand = Dielectric('Water saturated sand (min)', 10, 30, 30, 1, 0.1*1e-03, 1, 30, 30, 1, 1) # Dry sand or gravel have this, bok av  bok av Seidel, Lange. 2007 (Knodel e.d.) p. 311. 
-	
-	Saturated_gravel = Dielectric('Water saturated gravel', 10, 30, 30, 1, 1/50, 1, 30, 30, 1, 1) # Dry sand or gravel have this, bok av Seidel, Lange. 2007 (Knodel e.d.)  p 238. 
-	TNT_non_disp = Dielectric('Non-dispersive TNT', 20, 1, 1, 1, 1/(8.97e10*1e-02), 1, 2.849, 2.849, 1, 1) # Walbrecht, T is unknown, we assumed ambient 20 deg C.
-	Tulle_is = Dielectric(r'Test ice', 100-273, 3.1, 3.1, 1.0, 1e-12, 1.0, 196.0, 3.1, 1/(2*np.pi*0.03), 1.0) # Grimm 2008, Johari 1975. --> orginale
-	
-	
-	Leire_kvikk_20_Ohmm_6kHz = Dielectric(r'Dispersive clay. $\rho=20~\Omega$m, $f_r=6$ kHz', 283-273, 3,   3,  1.0, 1/20,  1.0, 2000, 3, 6e03, 0.75) # efi
-	Leire_kvikk_30_Ohmm_6kHz = Dielectric(r'Dispersive clay. $\rho=30~\Omega$m, $f_r=6$ kHz', 283-273, 3,   3,  1.0, 1/30,  1.0, 2000, 3, 6e03, 0.75) # efi
-	Leire_kvikk_70_Ohmm_6kHz = Dielectric(r'Dispersive clay. $\rho=70~\Omega$m, $f_r=6$ kHz', 283-273, 3,   3,  1.0, 1/70,  1.0, 2000, 3, 6e03, 0.75) # efi
-	Leire_kvikk_120_Ohmm_6kHz = Dielectric(r'Dispersive clay. $\rho=120~\Omega$m, $f_r=6$ kHz', 283-273, 3,   3,  1.0, 1/120,  1.0, 2000, 3, 6e03, 0.75) # efi
-
-	Leire_kvikk_20_Ohmm_9kHz = Dielectric(r'Dispersive clay. $\rho=20~\Omega$m, $f_r=9$ kHz', 283-273, 3,   3,  1.0, 1/20,  1.0, 2000, 3, 9e03, 0.75) # efi
-	Leire_kvikk_30_Ohmm_9kHz = Dielectric(r'Dispersive clay. $\rho=30~\Omega$m, $f_r=9$ kHz', 283-273, 3,   3,  1.0, 1/30,  1.0, 2000, 3, 9e03, 0.75) # efi
-	Leire_kvikk_70_Ohmm_9kHz = Dielectric(r'Dispersive clay. $\rho=70~\Omega$m, $f_r=9$ kHz', 283-273, 3,   3,  1.0, 1/70,  1.0, 2000, 3, 9e03, 0.75) # efi
-	Leire_kvikk_120_Ohmm_9kHz = Dielectric(r'Dispersive clay. $\rho=120~\Omega$m, $f_r=9$ kHz', 283-273, 3,   3,  1.0, 1/120,  1.0, 2000, 3, 9e03, 0.75) # efi
-
-	
-	Leire_kvikk_20_Ohmm_10kHz = Dielectric(r'Dispersive clay. $\rho=20~\Omega$m, $f_r=10$ kHz', 283-273, 3,   3,  1.0, 1/20,  1.0, 2000, 3, 10e03, 0.75) # efi
-	Leire_kvikk_30_Ohmm_10kHz = Dielectric(r'Dispersive clay. $\rho=30~\Omega$m, $f_r=10$ kHz', 283-273, 3,   3,  1.0, 1/30,  1.0, 2000, 3, 10e03, 0.75) # efi
-	Leire_kvikk_70_Ohmm_10kHz = Dielectric(r'Dispersive clay. $\rho=70~\Omega$m, $f_r=10$ kHz', 283-273, 3,   3,  1.0, 1/70,  1.0, 2000, 3, 10e03, 0.75) # efi
-	Leire_kvikk_120_Ohmm_10kHz = Dielectric(r'Dispersive clay. $\rho=120~\Omega$m, $f_r=10$ kHz', 283-273, 3,   3,  1.0, 1/120,  1.0, 2000, 3, 10e03, 0.75) # efi
-
 			#	name,          T,     perm0, perm8, f_r, cond_dc, c, perm0_2, perm8_2, f_r_2, c_2
 
-				# tregest rel. saa raskest rel. ...
 
-	return ice3, ice10, ice30, RedBluffClay1, RedBluffClay3, LSoilSand3, WallaWallaSiltLoam3, TNT, Regolith, Regolith_1e10, Permafrost_from_inversion, Hundre, To_hundre, To_hundre_og_femti, Fire_hundre_og_femti, Tusen, Tre_tusen, Ti_tusen, Ti_i_tiende, WallaWallaSiltLoam_dc, air, permafrost_model_2008, permafrost_model_2008_c_0999, Fairbanks_silt_100_ice_minus_3, Fairbanks_silt_72_ice_minus_3, Fairbanks_silt_44_ice_minus_3, Fairbanks_silt_72_ice_minus_3_one_rel, Sand_ice_46_ice_181K, Sand_ice_46_ice_181K_hydrate_static_1e04_c_0_3, Sand_ice_46_ice_181K_slow, Sand_ice_46_ice_181K_slower,  Sand_ice_46_ice_100K_fake, Sand_ice_46_ice_100K_c_95, Sand_ice_46_ice_100K, Sand_ice_17_ice_100K, Sand_ice_7_ice_100K_lower_cond, Sand_ice_7_ice_100K_faster_tau, Sand_ice_7_ice_100K, Sand_ice_46_ice_100K_tau_1em1, Sand_ice_46_ice_100K_tau_1em2, Sand_ice_46_ice_100K_tau_1e4, JSC_Mars_1, Ice_0_1M_NaCl_210K, Ice_1mM_NaCl_210K, Ice_0_1M_NaCl_100K, Ice_0_1M_NaCl_198K, Ice_0_1M_NaCl_100K_099, Ice_0_1M_NaCl_100K_0999, Ice_0_1M_NaCl_100K_09999, Ice_0_1M_NaCl_100K_099999, Ice_0_1M_NaCl_100K_0999999, Ice_0_1M_NaCl_100K_05, JSC_Mars_1_60_perc_ice_0_1M_CaCl2_210K, JSC_Mars_1_60_perc_ice_0_1M_CaCl2_198K, c_test, c_test_c_0999, num_test_c_stab_c_0999, num_test_c_stab_c_1, num_test_c_stab_c_076, Sand_ice_46_ice_100K_c075, Saturated_gravel, TNT_non_disp, Tulle_is, Dry_sand, Water_sat_high_sand, Water_sat_low_sand, Bedrock, Leire_kvikk_20_Ohmm_6kHz, Leire_kvikk_70_Ohmm_6kHz, Leire_kvikk_120_Ohmm_6kHz, Leire_kvikk_30_Ohmm_6kHz, Leire_kvikk_20_Ohmm_9kHz, Leire_kvikk_70_Ohmm_9kHz, Leire_kvikk_120_Ohmm_9kHz, Leire_kvikk_30_Ohmm_9kHz, Leire_kvikk_20_Ohmm_10kHz, Leire_kvikk_70_Ohmm_10kHz, Leire_kvikk_120_Ohmm_10kHz, Leire_kvikk_30_Ohmm_10kHz, Very_low_non_dispersive
-
+	return Regolith, Regolith_1e10, Permafrost_from_inversion, Hundre, To_hundre, To_hundre_og_femti, Fire_hundre_og_femti, Tusen, Tre_tusen, Ti_tusen, Ti_i_tiende, WallaWallaSiltLoam_dc, air, permafrost_model_2008, permafrost_model_2008_c_0999, Fairbanks_silt_100_ice_minus_3, Fairbanks_silt_72_ice_minus_3, Fairbanks_silt_44_ice_minus_3, Fairbanks_silt_72_ice_minus_3_one_rel, Sand_ice_46_ice_181K, Sand_ice_46_ice_181K_hydrate_static_1e04_c_0_3, Sand_ice_46_ice_181K_slow, Sand_ice_46_ice_181K_slower,  Sand_ice_46_ice_100K_fake, Sand_ice_46_ice_100K_c_95, Sand_ice_46_ice_100K, Sand_ice_17_ice_100K, Sand_ice_7_ice_100K_lower_cond, Sand_ice_7_ice_100K_faster_tau, Sand_ice_7_ice_100K, Sand_ice_46_ice_100K_tau_1em1, Sand_ice_46_ice_100K_tau_1em2, Sand_ice_46_ice_100K_tau_1e4, JSC_Mars_1, Ice_0_1M_NaCl_210K, Ice_1mM_NaCl_210K, Ice_0_1M_NaCl_100K, Ice_0_1M_NaCl_198K, Ice_0_1M_NaCl_100K_099, Ice_0_1M_NaCl_100K_0999, Ice_0_1M_NaCl_100K_09999, Ice_0_1M_NaCl_100K_099999, Ice_0_1M_NaCl_100K_0999999, Ice_0_1M_NaCl_100K_05, JSC_Mars_1_60_perc_ice_0_1M_CaCl2_210K, JSC_Mars_1_60_perc_ice_0_1M_CaCl2_198K, c_test, c_test_c_0999, num_test_c_stab_c_0999, num_test_c_stab_c_1, num_test_c_stab_c_076, Sand_ice_46_ice_100K_c075
 
 
-# Bittellli applies b not c: therefore c = (1 - b)
-
-#	When b is equal to zero, the material has a
-#single characteristic relaxation frequency or time (original
-#Debye [1929] model).
-
-# i.e. c = 1 for Debye, and c = 1 - b
 
 	
 			
